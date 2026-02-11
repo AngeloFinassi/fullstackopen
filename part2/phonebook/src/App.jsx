1e02785c991d0d12f8029c3bc4adce1e4a963320
Ângelo Matins Finassi <159829035+AngeloFinassi@users.noreply.github.com>
2026-02-11 19:44:45 -0300

    made phonebook

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,54 +1,158 @@
-const Header = (props) => {
-  return <h1>{props.course}</h1>
+import { useState } from 'react'
+
+const Filter = ({
+  filterName,
+  handleFilterChange,
+  handleFind
+}) => {
+  return (
+    <form onSubmit={handleFind}>
+      <div>
+        filter shown with:{' '}
+        <input
+          value={filterName}
+          onChange={handleFilterChange}
+        />
+        <button type="submit">find</button>
+      </div>
+    </form>
+  )
 }
 
-const Part = ({name, exercises}) => {
-  return <p>{name} {exercises}</p>
+const PersonForm = ({
+  addContact,
+  newName,
+  handleNameChange,
+  newNumber,
+  handleNumberChange
+}) => {
+  return (
+    <form onSubmit={addContact}>
+      <div>
+        name:{' '}
+        <input
+          value={newName}
+          onChange={handleNameChange}
+        />
+      </div>
+
+      <div>
+        number:{' '}
+        <input
+          value={newNumber}
+          onChange={handleNumberChange}
+        />
+      </div>
+
+      <button type="submit">add</button>
+    </form>
+  )
 }
 
-const Content = (props) => {
+const Persons = ({ persons }) => {
   return (
     <div>
-      <Part name={props.part1} exercises={props.exercises1} />
-      <Part name={props.part2} exercises={props.exercises2} />
-      <Part name={props.part3} exercises={props.exercises3} />
+      {persons.map(person => (
+        <p key={person.name}>
+          {person.name} {person.number}
+        </p>
+      ))}
     </div>
   )
 }
 
-const Total = (props) => {
-  return (
-    <p>
-      Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}
-    </p>
-  )
-}
-
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '1234' }
+  ])
+
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [filterName, setFilterName] = useState('')
+
+  const addContact = (event) => {
+    event.preventDefault()
+
+    if (newName === '' || newNumber === '') {
+      alert('Name and number cannot be empty')
+      return
+    }
+
+    if (persons.some(p => p.name === newName)) {
+      alert(`${newName} is already added to phonebook`)
+      return
+    }
+
+    setPersons(persons.concat({
+      name: newName,
+      number: newNumber
+    }))
+
+    setNewName('')
+    setNewNumber('')
+  }
+
+  const handleFind = (event) => {
+    event.preventDefault()
+
+    if (filterName === '') {
+      alert('Type a name to search')
+      return
+    }
+
+    const found = persons.find(
+      person =>
+        person.name.toLowerCase() ===
+        filterName.toLowerCase()
+    )
+
+    if (found) {
+      alert(`${found.name} ${found.number}`)
+    } else {
+      alert(`${filterName} not found`)
+    }
+
+    setFilterName('')
+  }
+
+  const personsToShow =
+    filterName === ''
+      ? persons
+      : persons.filter(person =>
+          person.name
+            .toLowerCase()
+            .includes(filterName.toLowerCase())
+        )
 
   return (
     <div>
-      <Header course={course}></Header>
-      <Content
-        part1={part1}
-        exercises1={exercises1}
-        part2={part2}
-        exercises2={exercises2}
-        part3={part3}
-        exercises3={exercises3}
-      ></Content>
-      <Total 
-        exercises1={exercises1}
-        exercises2={exercises2}
-        exercises3={exercises3}
-      ></Total>
+      <h2>Phonebook</h2>
+
+      <Filter
+        filterName={filterName}
+        handleFilterChange={(e) =>
+          setFilterName(e.target.value)
+        }
+        handleFind={handleFind}
+      />
+
+      <h3>Add a new</h3>
+
+      <PersonForm
+        addContact={addContact}
+        newName={newName}
+        handleNameChange={(e) =>
+          setNewName(e.target.value)
+        }
+        newNumber={newNumber}
+        handleNumberChange={(e) =>
+          setNewNumber(e.target.value)
+        }
+      />
+
+      <h3>Numbers</h3>
+
+      <Persons persons={personsToShow} />
     </div>
   )
 }
